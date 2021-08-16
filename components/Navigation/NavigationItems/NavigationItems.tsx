ce96ead494c8b63235871b2c3549b567b064bd70
Jyotiranjan Sahoo <jsahoo.p@gmail.com>
2021-08-16 17:24:54 +0530

    added organization intro page

--- components/Navigation/NavigationItems/NavigationItems.tsx
+++ components/Navigation/NavigationItems/NavigationItems.tsx
@@ -3,14 +3,14 @@ import NavigationItem from './NavigationItem/NavigationItem';
 import styles from './NavigationItems.module.scss';
 
 type Props = {
-  clicked: () => void;
+  clicked?: () => void;
 };
 
 export default function NavigationItems({ clicked }: Props) {
   return (
     <ul className={styles.NavigationItems} onClick={clicked}>
       <NavigationItem href="/">Tournaments</NavigationItem>
-      <NavigationItem href="/history">History</NavigationItem>
+      <NavigationItem href="/organizations">Organizations</NavigationItem>
       <NavigationItem href="/about">About</NavigationItem>
       <NavigationItem href="/profile">Profile</NavigationItem>
     </ul>
---
+++ components/Organization/OrganizationsIntro/OrganizationIntro.tsx
@@ -0,0 +1,103 @@
+import {
+  Button,
+  CardContent,
+  CardHeader,
+  Container,
+  ListItem,
+  Typography,
+} from '@material-ui/core';
+import { grey } from '@material-ui/core/colors';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { AddRounded } from '@material-ui/icons';
+import React from 'react';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      maxWidth: 1000,
+    },
+    header: {
+      letterSpacing: 1,
+      color: grey[700],
+    },
+    buttonContainer: {
+      marginTop: 50,
+    },
+    buttonText: {
+      fontWeight: 'bold',
+      letterSpacing: 0.5,
+    },
+    cardContainer: {
+      marginTop: 50,
+    },
+  })
+);
+
+export default function NoOrganization() {
+  const styles = useStyles();
+
+  return (
+    <Container className={styles.root}>
+      <Typography
+        variant="h6"
+        component="h2"
+        gutterBottom
+        className={styles.header}
+      >
+        Creating your organization page is just simple clicks away.
+      </Typography>
+      <div className={styles.buttonContainer}>
+        <Button
+          variant="outlined"
+          endIcon={<AddRounded color="primary" fontSize="large" />}
+        >
+          <Typography
+            variant="body1"
+            color="primary"
+            className={styles.buttonText}
+          >
+            Create Your Organization Page
+          </Typography>
+        </Button>
+      </div>
+      <div className={styles.cardContainer}>
+        <CardHeader title="Who is it for?" />
+        <CardContent>
+          <ListItem>
+            <Typography variant="subtitle1" color="textPrimary">
+              1. Are you organizing Custom room matches?
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <Typography variant="subtitle1" color="textPrimary">
+              2. Are you a Tournament organizer?
+            </Typography>
+          </ListItem>
+        </CardContent>
+        <Typography variant="h6" color="textPrimary">
+          Create your organization page and start registration of custom matches
+          for
+        </Typography>
+        <ListItem>
+          <Typography variant="button" color="textPrimary">
+            1. Battle Grounds Mobile India / PUBG Mobile
+          </Typography>
+        </ListItem>
+        <ListItem>
+          <Typography variant="button" color="textPrimary">
+            2. Call of Duty Mobile
+          </Typography>
+        </ListItem>
+        <ListItem>
+          <Typography variant="button" color="textPrimary">
+            3. Garena Free Fire
+          </Typography>
+        </ListItem>
+        <br />
+        <Typography variant="body1" color="textSecondary">
+          We will be adding more games very soon.
+        </Typography>
+      </div>
+    </Container>
+  );
+}

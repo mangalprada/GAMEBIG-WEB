43a2fdd22b19293763ece6d002f5cdb98abb476e
mmalay <malaydsg@gmail.com>
2021-09-11 10:26:57 +0530

    environment variables setup

--- firebase/config.ts
+++ firebase/config.ts
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const config = {
-  apiKey: 'AIzaSyBUuGX7ijAKLpAVvPEQb9lr_bmGEJuOJeo',
-  authDomain: 'dlord-dev.firebaseapp.com',
-  projectId: 'dlord-dev',
-  storageBucket: 'dlord-dev.appspot.com',
-  messagingSenderId: '634466289053',
-  appId: '1:634466289053:web:4ad89f9d81a26cdd311b74',
-  measurementId: 'G-JVNZ8B2MJ6',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
--- next.config.js
+++ next.config.js
@@ -8,4 +8,13 @@ module.exports = {
   images: {
     domains: ['lh3.googleusercontent.com', 'play-lh.googleusercontent.com'],
   },
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+  },
 };

9924409b5032cbacb03466a56398bd4e99c538b7
mmalay <malaydsg@gmail.com>
2021-09-15 18:13:20 +0530

    accessibility improved

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 
 module.exports = {
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'classes')],

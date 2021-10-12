c6fffafc718a4aff173dd12a5cf077fc7ab76eba
mmalay <malaydsg@gmail.com>
2021-10-13 01:54:54 +0530

    progressbar added

--- hoc/Layout/Layout.tsx
+++ hoc/Layout/Layout.tsx
@@ -1,6 +1,5 @@
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import BottomNavigationBar from '../../components/Navigation/NavigationItems/BottomNavigationBar';
-import Progress from '../../components/UI/Progress/Progress';
 
 type Props = {
   children: React.ReactNode;
@@ -9,7 +8,6 @@ type Props = {
 function Layout({ children }: Props) {
   return (
     <div className="bg-black">
-      <Progress />
       <Toolbar />
       <main className="sm:w-11/12 lg:w-4/5 bg-repeat justify-center mx-auto">
         {children}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,48 @@
+import { useEffect } from 'react';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import NProgress from 'nprogress';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
-import '../styles/globals.scss';
 import 'tailwindcss/tailwind.css';
+import '../styles/globals.scss';
+import '../public/npprogress.css';
 import { AuthProvider } from '../context/authContext';
 import Layout from '../hoc/Layout/Layout';
+import ProgressBar from '../components/UI/Progress/ProgressBar';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      console.log(`Loading: ${url}`);
+      console.log(`Loading: ${router.pathname}`);
+      NProgress.start();
+    };
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
   return (
     <AuthProvider>
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        {/* <ProgressBar /> */}
+        <style jsx>{`
+          a {
+            margin: 0 10px 0 0;
+          }
+        `}</style>
         <Layout>
           <Component {...pageProps} />
         </Layout>

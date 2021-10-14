b4fa7c003cf07674f7f0c3c499b5528d39f5d9e6
mmalay <malaydsg@gmail.com>
2021-10-14 18:50:31 +0530

    nav  bar done

--- components/Navigation/NavigationItems/NavigationItem/NavigationItem.tsx
+++ components/Navigation/NavigationItems/NavigationItem/NavigationItem.tsx
@@ -1,22 +1,41 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 type Props = {
   children: JSX.Element | string;
   href: string;
   toolTip?: string;
+  isActive?: boolean;
 };
 
-export default function NavigationItem({ children, href, toolTip }: Props) {
+export default function NavigationItem({
+  children,
+  href,
+  isActive,
+  toolTip,
+}: Props) {
+  const router = useRouter();
   return (
-    <li className="py-1 px-4 font-semibold rounded-lg hover:bg-gray-900 rou transition duration-300">
-      <div className="flex flex-col justify-center">
+    <li
+      className={
+        'font-semibold ' + (isActive ? 'border-b-4 border-indigo-600' : '')
+      }
+    >
+      <div
+        className={
+          'pt-1.5 px-6 flex flex-col justify-center ' +
+          (isActive
+            ? ''
+            : 'hover:bg-gray-900 rounded-t-lg transition duration-300')
+        }
+      >
         <div className="relative sm:max-w-xl sm:mx-auto">
           <div className="group cursor-pointer relative inline-block text-center">
             <Link href={href} passHref>
               <a href={href}>{children}</a>
             </Link>
-            <div className="opacity-0 w-24 bg-gray-900 text-gray-300 text-center text-xs rounded-lg py-2 absolute z-10 group-hover:opacity-100 top-14 -left-7 px-2 pointer-events-none">
+            <div className="opacity-0 w-24 font-sans bg-gray-300 text-grey-900 text-center text-xs rounded-md py-2 absolute z-10 group-hover:opacity-100 top-14 -left-7 px-1.5 pointer-events-none">
               {toolTip}
             </div>
           </div>

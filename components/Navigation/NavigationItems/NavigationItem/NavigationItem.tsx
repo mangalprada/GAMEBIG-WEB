dedad0375842673a6dd11f89f3170f7b6eefa659
mmalay <malaydsg@gmail.com>
2021-10-14 16:41:37 +0530

    size fix

--- components/Navigation/NavigationItems/NavigationItem/NavigationItem.tsx
+++ components/Navigation/NavigationItems/NavigationItem/NavigationItem.tsx
@@ -11,11 +11,11 @@ export default function NavigationItem({ children, href }: Props) {
     <li className="py-1 px-4 font-semibold rounded-lg hover:bg-gray-900 rou transition duration-300">
       <div className="flex flex-col justify-center">
         <div className="relative sm:max-w-xl sm:mx-auto">
-          <div className="group cursor-pointer relative inline-block w-28 text-center">
+          <div className="group cursor-pointer relative inline-block text-center">
             <Link href={href} passHref>
               <a href={href}>{children}</a>
             </Link>
-            <div className="opacity-0 w-28 bg-gray-900 text-gray-300 text-center text-xs rounded-lg py-2 absolute z-10 group-hover:opacity-100 top-14 -left-1/2 ml-14 px-3 pointer-events-none">
+            <div className="opacity-0 w-28 bg-gray-900 text-gray-300 text-center text-xs rounded-lg py-2 absolute z-10 group-hover:opacity-100 top-14 -left-9 px-1 pointer-events-none">
               Tooltip center
             </div>
           </div>

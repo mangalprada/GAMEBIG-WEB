e48a579b0aad91aefe5ae5d08489338e98f44754
mmalay <malaydsg@gmail.com>
2021-10-14 17:10:40 +0530

    tooltip added

--- components/Navigation/NavigationItems/NavigationItem/NavigationItem.tsx
+++ components/Navigation/NavigationItems/NavigationItem/NavigationItem.tsx
@@ -4,9 +4,10 @@ import React from 'react';
 type Props = {
   children: JSX.Element | string;
   href: string;
+  toolTip?: string;
 };
 
-export default function NavigationItem({ children, href }: Props) {
+export default function NavigationItem({ children, href, toolTip }: Props) {
   return (
     <li className="py-1 px-4 font-semibold rounded-lg hover:bg-gray-900 rou transition duration-300">
       <div className="flex flex-col justify-center">
@@ -15,8 +16,8 @@ export default function NavigationItem({ children, href }: Props) {
             <Link href={href} passHref>
               <a href={href}>{children}</a>
             </Link>
-            <div className="opacity-0 w-28 bg-gray-900 text-gray-300 text-center text-xs rounded-lg py-2 absolute z-10 group-hover:opacity-100 top-14 -left-9 px-1 pointer-events-none">
-              Tooltip center
+            <div className="opacity-0 w-24 bg-gray-900 text-gray-300 text-center text-xs rounded-lg py-2 absolute z-10 group-hover:opacity-100 top-14 -left-7 px-2 pointer-events-none">
+              {toolTip}
             </div>
           </div>
         </div>

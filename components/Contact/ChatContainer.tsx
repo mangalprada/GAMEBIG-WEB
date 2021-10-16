9698e9a5e5a92f0af69f8b7c8512b48fedca2683
Jyotiranjan Sahoo <jsahoo.p@gmail.com>
2021-10-16 20:37:28 +0530

    modified chat UI

--- components/Contact/ChatContainer.tsx
+++ components/Contact/ChatContainer.tsx
@@ -25,7 +25,12 @@ export default function ChatContainer({ chatDatas }: Props) {
     });
 
   return (
-    <div className="flex flex-col bg-white rounded-t-md h-96 mx-5 mt-5 overflow-y-scroll md:px-4">
+    <div
+      className={
+        'flex flex-col bg-gray-900 rounded-md h-96 mt-5 border-2 ' +
+        'border-gray-800 overflow-y-scroll px-2 md:px-4'
+      }
+    >
       {messages}
       <div ref={scrollLast}></div>
     </div>

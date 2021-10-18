49a3845a5200f334711d2a17bb59a4d1bb6eaad3
Jyotiranjan Sahoo <jsahoo.p@gmail.com>
2021-10-18 17:20:25 +0530

    modified profile page UI

---
+++ components/UI/Avatar/ProfileAvatar.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+import Image from 'next/image';
+
+type Props = {
+  photoURL?: string;
+};
+
+const ProfileAvatar: FC<Props> = ({ photoURL }: Props) => {
+  return (
+    <div
+      className={
+        'rounded-full relative h-36 w-36 border-4 bg-black border-black ' +
+        'flex flex-col justify-center'
+      }
+    >
+      {photoURL ? (
+        <Image
+          className="rounded-full"
+          src={photoURL}
+          alt="User Profile Pic"
+          layout="fill"
+          objectFit="contain"
+        />
+      ) : (
+        <span className="text-center tracking-wide text-gray-200 font-medium">
+          No Pic
+        </span>
+      )}
+      <div className="absolute"></div>
+    </div>
+  );
+};
+
+export default ProfileAvatar;

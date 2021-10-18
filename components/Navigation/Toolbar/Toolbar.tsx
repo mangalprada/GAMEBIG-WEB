c592d1acbfc8a43aea527cb7e6dd1d25a569fd92
Jyotiranjan Sahoo <jsahoo.p@gmail.com>
2021-10-18 17:46:13 +0530

    added sticky nav bar

--- components/Navigation/Toolbar/Toolbar.tsx
+++ components/Navigation/Toolbar/Toolbar.tsx
@@ -4,8 +4,8 @@ import SecondaryNavigationItems from '../NavigationItems/SecondaryNavigationItem
 
 function MainNavigation() {
   return (
-    <header>
-      <nav className="h-auto bg-gradient-to-b from-transparent via-transparent to-gray-900">
+    <header className="sticky top-0 z-50">
+      <nav className="h-auto bg-gradient-to-b from-black via-black to-gray-900">
         <div className="md:mx-6 mx-4">
           <div className="flex justify-between">
             <HeaderLogo />

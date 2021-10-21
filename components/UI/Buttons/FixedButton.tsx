2705c5d60a57edfff4dfb4519ee0ce7007badad8
mmalay <malaydsg@gmail.com>
2021-10-21 10:35:21 +0530

    fixed button updated

--- components/UI/Buttons/FixedButton.tsx
+++ components/UI/Buttons/FixedButton.tsx
@@ -9,22 +9,24 @@ const FixedButton: FC<ButtonProps> = ({
   isDangerous,
 }: ButtonProps) => {
   return (
-    <button
-      className={
-        'text-white font-normal tracking-wide text-lg py-2 px-4 rounded-lg ' +
-        (isDisabled
-          ? 'bg-gray-500 opacity-50 cursor-not-allowed'
-          : 'transition duration-300 ease-in-out shadow-lg hover:shadow-xl ' +
-            (isDangerous
-              ? 'bg-red-500 hover:bg-red-600'
-              : 'bg-indigo-600 hover:bg-indigo-700'))
-      }
-      type={type}
-      onClick={onClickHandler}
-      disabled={isDisabled}
-    >
-      {name}
-    </button>
+    <div>
+      <button
+        className={
+          'text-white font-normal tracking-wide text-lg py-2 px-4 rounded-lg ' +
+          (isDisabled
+            ? 'bg-gray-500 opacity-50 cursor-not-allowed'
+            : 'transition duration-300 ease-in-out shadow-lg hover:shadow-xl ' +
+              (isDangerous
+                ? 'bg-red-500 hover:bg-red-600'
+                : 'bg-indigo-600 hover:bg-indigo-700'))
+        }
+        type={type}
+        onClick={onClickHandler}
+        disabled={isDisabled}
+      >
+        {name}
+      </button>
+    </div>
   );
 };
 

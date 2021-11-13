430c66dbdf365c0af3106d25ea2a79d4b24a95d4
Mangalprada Malaya <malaydsg@gmail.com>
2021-11-13 15:24:47 +0530

    Sentry (#76)
    * sentry setup
    
    * next config modified
    
    * error fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const withPWA = require('next-pwa');
+const { withSentryConfig } = require('@sentry/nextjs');
 
-module.exports = withPWA({
+moduleExportsWithPWA = withPWA({
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
@@ -36,3 +37,22 @@ module.exports = withPWA({
     ALGOLIA_SEARCH_ONLY_API_KEY: process.env.ALGOLIA_SEARCH_ONLY_API_KEY,
   },
 });
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(
+  moduleExportsWithPWA,
+  SentryWebpackPluginOptions
+);

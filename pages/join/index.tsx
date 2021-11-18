1062b7e72c10c27da20d33cb9deb9ffb391c841c
Jyotiranjan Sahoo <57587698+jyotiranjan97@users.noreply.github.com>
2021-11-18 16:09:20 +0530

    Uxissues (#102)
    * added radix acordian to package
    
    * added faq for organization
    
    * modified feedback
    
    * increase margin in intro page
    
    * added more info on join screen
    
    * improved event card ux
    
    * modified message container position

--- pages/join/index.tsx
+++ pages/join/index.tsx
@@ -8,6 +8,8 @@ import CreatePostForm from '@/components/Join/CreatePostForm';
 import { useAuth } from '@/context/authContext';
 import { JoinPostType } from '@/utilities/join/JoinPostType';
 import { firebaseAdmin } from 'firebase/firebaseAdmin';
+import Aux from 'hoc/Auxiliary/Auxiliary';
+import TeamUpFAQ from '@/components/Join/TeamUpFAQ';
 
 const JoinPage = ({ joinPosts }: { joinPosts: JoinPostType[] }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -27,17 +29,20 @@ const JoinPage = ({ joinPosts }: { joinPosts: JoinPostType[] }) => {
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
-      <div>
+      <Aux>
+        <div className={'flex mt-3 md:w-2/3 xl:w-1/2 sm:mx-auto mx-2'}>
+          <TeamUpFAQ />
+        </div>
         <div
           className={
             'flex justify-between rounded-lg bg-gray-900 h-14 ' +
-            'md:w-2/3 xl:w-1/2 sm:mx-auto mx-5 mt-4 mb-5 px-8 py-2 relative '
+            'md:w-2/3 xl:w-1/2 sm:mx-auto mx-3 mt-4 mb-3 px-8 py-2 relative'
           }
         >
           <span
             className={
               'text-lg font-semibold tracking-wide text-gray-300 absolute ' +
-              'cursor-pointer hover:text-indigo-600 hover:bg-indigo-200 ' +
+              'cursor-pointer hover:text-indigo-600 bg-gray-800/50 hover:bg-indigo-200 ' +
               'px-3 py-1.5 top-2 left-2.5 rounded-md active:bg-indigo-300'
             }
             onClick={goToMyPosts}
@@ -52,20 +57,18 @@ const JoinPage = ({ joinPosts }: { joinPosts: JoinPostType[] }) => {
             }
             onClick={() => setIsModalOpen(true)}
           >
-            Team Up +
+            Find Teammate
           </span>
         </div>
         <div>
           {joinPosts.map((joinPost) => (
-            <div key={joinPost.id}>
-              <TeamUpItem data={joinPost} />
-            </div>
+            <TeamUpItem data={joinPost} key={joinPost.id} />
           ))}
         </div>
         <Modal isOpen={isModalOpen} closeModal={() => setIsModalOpen(false)}>
           <CreatePostForm closeModal={() => setIsModalOpen(false)} />
         </Modal>
-      </div>
+      </Aux>
     </div>
   );
 };

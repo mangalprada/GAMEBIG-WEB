c51a583ff445881ab6f46793dc19ff69418d17bd
Mangalprada Malaya <malaydsg@gmail.com>
2021-11-18 11:38:15 +0530

    next config (#92)
    * next config
    
    * next config
    
    * next config
    
    * next config 3
    
    * next config 4

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 const path = require('path');
 const withPWA = require('next-pwa');
 
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-const { withSentryConfig } = require('@sentry/nextjs');
-
-const moduleExports = withPWA({
+module.exports = withPWA({
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
@@ -18,10 +11,6 @@ const moduleExports = withPWA({
     locales: ['en'],
     defaultLocale: 'en',
   },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'classes')],
-  },
-  outputFileTracing: false,
   images: {
     domains: [
       'lh3.googleusercontent.com',
@@ -41,21 +30,6 @@ const moduleExports = withPWA({
     FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
     ALGOLIA_APPLICATION_ID: process.env.ALGOLIA_APPLICATION_ID,
     ALGOLIA_SEARCH_ONLY_API_KEY: process.env.ALGOLIA_SEARCH_ONLY_API_KEY,
+    SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
   },
 });
-
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);

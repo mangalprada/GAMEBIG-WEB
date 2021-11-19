0c28e061bff01e272efc7db369255e5dbcf1203e
Mangalprada Malaya <malaydsg@gmail.com>
2021-11-20 03:39:13 +0530

    Sentry (#108)
    * next config
    
    * sentry setup
    
    * sentry setup
    
    * sentry

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -16,5 +16,6 @@ Sentry.init({
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
-  environment: 'production',
+  // environment: 'production',
+
 });

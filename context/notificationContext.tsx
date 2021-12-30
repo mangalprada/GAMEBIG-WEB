1a67b140876b174b9bcbe5b6c07a5fe66da0c0f4
mmalay <malaydsg@gmail.com>
2021-12-30 13:37:50 +0530

    service worker error fixing

--- context/notificationContext.tsx
+++ context/notificationContext.tsx
@@ -46,13 +46,14 @@ function useProviderNotification() {
 
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      try {
-        navigator.serviceWorker.register('firebase-messaging-sw.js', {
-          scope: '/',
+      navigator.serviceWorker
+        .register('./firebase-messaging-sw.js')
+        .then(function (registration) {
+          console.log('Registration successful, scope is:', registration.scope);
+        })
+        .catch(function (err) {
+          console.log('Service worker registration failed, error:', err);
         });
-      } catch (err) {
-        console.log(err);
-      }
     }
   }, []);
 

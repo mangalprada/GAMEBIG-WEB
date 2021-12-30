657a2f49cd4a5318137807d9934632bbe8ef9126
mmalay <malaydsg@gmail.com>
2021-12-30 14:51:51 +0530

    Event link fixing

--- pages/profile/[username]/index.tsx
+++ pages/profile/[username]/index.tsx
@@ -52,7 +52,7 @@ export default function Events({ events, userData }: Props) {
       <Aux>
         <ProfileHeader userData={userData} />
         <div className="mt-10">
-          {events.length > 0 ? allParticipatedEvents : emptyEventsComponent}
+          {/* //Todo {events.length > 0 ? allParticipatedEvents : emptyEventsComponent} */}
         </div>
       </Aux>
     </div>

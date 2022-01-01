de70907304ffc1a7605901f8c5704ed4be5e8947
mmalay <malaydsg@gmail.com>
2022-01-01 13:50:53 +0530

    mix panel added

--- mixpanel/Mixpanel.ts
+++ mixpanel/Mixpanel.ts
@@ -1,29 +1,4 @@
-import mixpanel, { Dict } from 'mixpanel-browser';
-
+import mixpanel from 'mixpanel-browser';
 const token = process.env.MIXPANEL_TOKEN;
-if (token) mixpanel.init(token, { ignore_dnt: true, track_pageview: true });
-
-const env_check = process.env.NODE_ENV === 'production';
-
-const actions = {
-  identify: (id: string) => {
-    if (env_check) mixpanel.identify(id);
-  },
-  alias: (id: string) => {
-    if (env_check) mixpanel.alias(id);
-  },
-  track: (name: string, props?: Dict | undefined) => {
-    if (env_check) mixpanel.track(name, props);
-  },
-  people: {
-    set: (props: Dict) => {
-      if (env_check) mixpanel.people.set(props);
-    },
-  },
-  // super properties
-  register: (props: Dict) => {
-    if (env_check) mixpanel.register(props);
-  },
-};
-
-export const Mixpanel = actions;
+if (token) mixpanel.init(token, { debug: true, ignore_dnt: true });
+export default mixpanel;

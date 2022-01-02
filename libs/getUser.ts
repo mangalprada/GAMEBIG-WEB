baf4e32fc376afd79b2b614c2c9f53211c347128
Mangalprada Malaya <malaydsg@gmail.com>
2022-01-02 21:37:34 +0530

    Auth (#140)
    * minor change
    
    * date fix

--- libs/getUser.ts
+++ libs/getUser.ts
@@ -13,9 +13,7 @@ const getUser = async (username: string) => {
         if (data) {
           user.push({
             ...data,
-            dob: data.dob
-              ? data.dob.toDate().toISOString()
-              : new Date().toISOString(),
+            dob: data.dob ? data.dob.toDate().toISOString() : null,
             docId: doc.id,
           });
         }

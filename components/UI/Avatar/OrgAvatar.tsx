b2c7966a8c6e38a53d242f72a9bbfd13067d0555
mmalay <malaydsg@gmail.com>
2022-02-18 21:04:20 +0530

    ui fix

--- components/UI/Avatar/OrgAvatar.tsx
+++ components/UI/Avatar/OrgAvatar.tsx
@@ -18,31 +18,34 @@ const EventCardAvatar: FC<Props> = ({
   openEditModal,
 }: Props) => {
   return (
-    <div
-      className="relative items-center w-20 h-20 sm:w-28 sm:h-28 md:w-36 md:h-36 m-auto"
-      onClick={onclick}
-    >
-      {photoURL ? (
-        <Image
-          className="rounded-full"
-          src={photoURL}
-          alt="Org Profile Pic"
-          layout="fill"
-          objectFit="cover"
-        />
-      ) : (
-        <div className="flex justify-center m-auto w-full h-full bg-indigo-600 rounded-full cursor-pointer">
-          <span className="self-center text:lg sm:text-2xl font-bold tracking-wide text-gray-900 font-sans">
-            {content?.toString().toUpperCase()}
-          </span>
-        </div>
-      )}
+    <div>
+      <div
+        className="relative items-center w-20 h-20 sm:w-28 sm:h-28 md:w-36 md:h-36 m-auto"
+        onClick={onclick}
+      >
+        {photoURL ? (
+          <Image
+            className="rounded-full"
+            src={photoURL}
+            alt="Org Profile Pic"
+            layout="fill"
+            objectFit="cover"
+          />
+        ) : (
+          <div className="flex justify-center m-auto w-full h-full bg-indigo-600 rounded-full cursor-pointer">
+            <span className="self-center text:lg sm:text-2xl font-bold tracking-wide text-gray-900 font-sans">
+              {content?.toString().toUpperCase()}
+            </span>
+          </div>
+        )}
+      </div>
       {isEditable && openEditModal ? (
         <div
-          className="fixed mt-16 sm:mt-24 md:mt-28 ml-7 sm:ml-10 md:ml-14 bg-gray-800 ring ring-indigo-700 rounded-full p-0.5 md:p-1"
+          className="flex justify-center items-center text-sm bg-gray-800 ring ring-indigo-800 
+          rounded-full p-0.5 mt-1.5 gap-1 text-gray-400 hover:ring-indigo-600 font-semibold "
           onClick={() => openEditModal()}
         >
-          <CameraIcon size={24} />
+          Edit <CameraIcon size={16} />
         </div>
       ) : null}
     </div>

b35239faa9f4a02f396f578e5dc90e0ef4a1082a
mmalay <malaydsg@gmail.com>
2022-02-23 16:55:59 +0530

    timedate picker fix

--- components/UI/Picker/TimeDatePicker.tsx
+++ components/UI/Picker/TimeDatePicker.tsx
@@ -1,11 +1,6 @@
-import { useState, forwardRef } from 'react';
+import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-
-type Props = {
-  onClick: () => void;
-  value: string;
-};
 
 const NewDatePicker = ({
   error,
@@ -23,49 +18,21 @@ const NewDatePicker = ({
     new Date(initialTime as Date) || new Date()
   );
 
-  const CustomInput = forwardRef<HTMLInputElement, Props>(
-    ({ value, onClick }, ref) => (
-      <input
-        className={
-          'border-0 px-3 py-3 font-sans cursor-pointer ' +
-          'rounded text-lg font-medium shadow w-full ' +
-          'placeholder-gray-300 placeholder-opacity-40 ' +
-          'ease-linear transition-all duration-150 bg-gray-700 text-gray-50 ' +
-          (error
-            ? 'focus:outline-none ring ring-red-500'
-            : 'focus:outline-none focus:ring focus:ring-indigo-500')
-        }
-        onClick={onClick}
-        onChange={() => console.log('')}
-        ref={ref}
-        value={value}
-        defaultValue={value}
-      />
-    )
-  );
-  CustomInput.displayName = 'CustomInput';
-
   return (
     <div>
       <label className="block uppercase text-gray-500 text-sm font-bold font-sans tracking-wide mb-2">
         {label}
       </label>
       <DatePicker
+        className="bg-gray-700 text-gray-50 font-semibold focus:outline-none w-full rounded-md h-12 px-3 focus:ring focus:ring-indigo-500"
         showTimeSelect
         dateFormat="MMMM d, yyyy h:mm aa"
         selected={startDate}
+        timeIntervals={initialTime ? 1 : 30}
         onChange={(date) => {
           changeHandler(date as Date);
           setStartDate(date as Date);
         }}
-        customInput={
-          <CustomInput
-            onClick={() => {
-              console.log('');
-            }}
-            value={''}
-          />
-        }
       />
     </div>
   );
